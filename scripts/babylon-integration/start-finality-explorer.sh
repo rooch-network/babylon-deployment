373213e075309070bde530f219fc7ce5762c6571
parketh <yeungparkhay@gmail.com>
2024-10-28 18:11:04 +0000

    fix: finality explorer (#18)
    * fix: use https
    
    * fix: load env vars
    
    * fix: set env vars
    
    * use new docker image
    
    * fix: docker compose env
    
    * update finality explorer image
    
    * update docker image
    
    * set docker image back to latest
    
    * use tagged finality-explorer image
    
    ---------
    
    Co-authored-by: bap2pecs <bap2pecs@proton.me>

--- scripts/babylon-integration/start-finality-explorer.sh
+++ scripts/babylon-integration/start-finality-explorer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# Load environment variables
+set -a
+source $(pwd)/.env.babylon-integration
+set +a
+
 echo "Starting finality explorer..."
 docker compose -f docker/docker-compose-babylon-integration.yml up -d finality-explorer
 
--- scripts/babylon-integration/stop-finality-explorer.sh
+++ scripts/babylon-integration/stop-finality-explorer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# Load environment variables
+set -a
+source $(pwd)/.env.babylon-integration
+set +a
+
 echo "Stopping finality explorer..."
 docker compose -f docker/docker-compose-babylon-integration.yml down -v finality-explorer
 
