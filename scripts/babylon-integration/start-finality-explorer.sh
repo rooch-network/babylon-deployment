271abe3f0f97743ca3fa281ff92b9b673dad6e78
bap2pecs <bap2pecs@proton.me>
2024-10-29 22:05:56 +0800

    chore: remove redundant env vars loading

--- scripts/babylon-integration/start-finality-explorer.sh
+++ scripts/babylon-integration/start-finality-explorer.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# Load environment variables
-set -a
-source $(pwd)/.env.babylon-integration
-set +a
-
 echo "Starting finality explorer..."
 docker compose -f docker/docker-compose-babylon-integration.yml up -d finality-explorer
 
--- scripts/babylon-integration/stop-finality-explorer.sh
+++ scripts/babylon-integration/stop-finality-explorer.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# Load environment variables
-set -a
-source $(pwd)/.env.babylon-integration
-set +a
-
 echo "Stopping finality explorer..."
 docker compose -f docker/docker-compose-babylon-integration.yml down -v finality-explorer
 
