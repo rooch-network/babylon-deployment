0c1906a039978cf03ae6fd60b4eee687a8bdeff9
lesterli <fulfilself@gmail.com>
2024-10-31 10:00:33 +0800

    fix: install the shared library libwasmvm for babylond (#23)

--- scripts/babylon-integration/stop-btc-staker.sh
+++ scripts/babylon-integration/stop-btc-staker.sh
@@ -5,7 +5,7 @@ echo "Stopping btc-staker..."
 docker compose -f docker/docker-compose-babylon-integration.yml down btc-staker
 
 echo "Removing btc-staker directory..."
-rm -rf $(pwd)/.btc-staker
+sudo rm -rf $(pwd)/.btc-staker
 
 echo "Stopped btc-staker"
 echo
--- scripts/babylon-integration/stop-consumer-eotsmanager.sh
+++ scripts/babylon-integration/stop-consumer-eotsmanager.sh
@@ -5,7 +5,7 @@ echo "Stopping consumer-eotsmanager..."
 docker compose -f docker/docker-compose-babylon-integration.yml down consumer-eotsmanager
 
 echo "Removing consumer-eotsmanager directory..."
-rm -rf $(pwd)/.consumer-eotsmanager
+sudo rm -rf $(pwd)/.consumer-eotsmanager
 
 echo "Stopped consumer-eotsmanager"
 echo
